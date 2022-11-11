043ea655099fc40ebe13a22f535eab3d73ed0e67
karolskiba <skiba.kar@gmail.com>
2022-11-11 16:39:40 +0100

    Filtering & Rendering image on hovered items

--- src/Components/PokemonFilter.js
+++ src/Components/PokemonFilter.js
@@ -1,4 +1,6 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { getPokemon } from '../getPokemon'
 import { getQuizItems } from '../getQuizItems'
 
 
@@ -6,36 +8,73 @@ export default function PokemonFilter() {
 
     const [listItems, setListItems] = useState( [] )
     const [isLoading, setIsLoading] = useState(true)
+    const [activeElement, setActiveElement] = useState( {
+                    name: "",
+                    addressUrl: ""
+                    })
+
     const [query, setQuery] = useState("")
+    const [photo, setPhoto] = useState("")
+
+
     const fetchItems = async () =>{
         await getQuizItems(151)
         .then( (response) => {
-            setListItems([...response.results])
-            console.log(listItems)
+            setListItems( [...response.results] )
+            console.log( listItems )
         })
     }
 
 
     useEffect( () => {
-        
         fetchItems()
         setIsLoading(false)
     }, [])
 
+    const fetchPhoto = async (url) =>{
+        await axios.get(url)
+              .then( (res) => {
+                setPhoto(res.data.sprites.front_default)
+              })
+    }
 
     return (
-    <div>
+    <div className='filter-container'>
         <input 
             placeholder='StartTyping' 
             onChange={  (e)=> {
                 setQuery(e.target.value)
             }} 
             type = "search"
-            value = {query}
+            value = { query }
         />
         <ul className='list-to-filter'>
-            {listItems.filter( (el) => el.name.includes(query)).map( (el) => {return <li>{el.name}</li>} 
-        )}
+            {listItems.filter( 
+                (el) => el.name.toLowerCase()
+                .includes( query.toLowerCase() ))
+                .map( (el) => {
+                    return <li 
+                        onMouseEnter={(e)=>{
+                        setPhoto("")
+                        setActiveElement({
+                                name:e.target.innerText.split(" ")[0].toLowerCase(),
+                                addressUrl: el.url
+                                })
+                            }}
+                        key = {el.name}
+                       
+                        >
+                        <div className = {activeElement.name === el.name ? "active-list-item" : "not-active-list-item"}>
+                            <div className = "optional-photo" 
+                                onClick={(e)=> {
+                                        fetchPhoto(activeElement.addressUrl)               
+                                }}>Show Image</div>
+
+                            <img src= {photo} alt = ""></img>
+                        </div>
+                        { el.name }</li>} 
+                )
+            }
     </ul>
         
     </div>
--- src/getPokemon.js
+++ src/getPokemon.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 const REACT_APP_API_URI = 'https://pokeapi.co/api/v2/'
 
 
-export const getPokemon = ( id) => {
+export const getPokemon = ( id ) => {
     const url = `${REACT_APP_API_URI}pokemon/${id}`
+
     return axios.get(url)
     .then( (response)=> response.data)
     
